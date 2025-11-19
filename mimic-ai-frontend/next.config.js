b8ede503d7e0f18bcbab82e5951dfa6a2be3191c
Jarivs <165238735+JarvisZhang24@users.noreply.github.com>
2025-11-18 19:54:16 -0500

    Remove mimic-frontend scaffolding files

---
+++ mimic-ai-frontend/next.config.js
@@ -0,0 +1,22 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+import "./src/env.js";
+
+/** @type {import("next").NextConfig} */
+const config = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  reactStrictMode: false,
+};
+
+export default config;
